9ef154eaee5c1d2b5fbfe41e8e44f191a5499348
Diana <diana.norva@yandex.ru>
2022-08-22 11:37:37 +0300

    Workshop

---
+++ Homework5/Program.cs
@@ -0,0 +1,49 @@
+﻿
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+class Program
+{
+    static int MaxNumberArray(int[] array)
+    {
+        int max = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+    static int MinNumberArray(int[] array)
+    {
+        int min = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+    static void Main(string[] args)
+    {
+        int[] array = {45,10,4,5,6,721,67,10,16,2345};
+        int max = MaxNumberArray(array);
+        int min = MinNumberArray(array);
+        Console.WriteLine(max);
+        Console.WriteLine(min);
+        Console.WriteLine(max - min);
+    }
+}
---
+++ Workshop/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+
+// [5, 18, 123, 6, 2] -> 1
+
+// [1, 2, 3, 6, 2] -> 0
+
+// [10, 11, 12, 13, 14] -> 5
+
+
+
+int[] array = new int[123];
+int count=0;
+
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
+        Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+
+for (int j = 0; j < array.Length; j++)
+    {
+       if(array[j] >= 10 & array[j] <= 99) 
+       count++;
+    }
+Console.WriteLine();
+Console.Write("Массив содержит следующее количество элементов из отрезка [10,99] - ");
+Console.Write(count);
+
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат запишите в новом массиве.
+
+// [1 2 3 4 5] -> 5 8 3
+
+// [6 7 3 6] -> 36 21
+
+int N = 10;
+int[] array = new int[N];
+
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        Console.Write(array[i] + ",");
+    }
+Console.WriteLine();
+
+int j = array.Length - 1;
+for (int i = 0; i < (array.Length + 1) % 2; i++)
+    {
+        Console.Write(array[i] * array[j] + ",");
+        j++;
+}
