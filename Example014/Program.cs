042a1f5eedd081575844a9bb152d55c3b4d7afa2
Diana <diana.norva@yandex.ru>
2022-08-30 16:19:41 +0300

    Example 014 new save

--- Example014/Program.cs
+++ Example014/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿/*
+
+using System.Linq;
 
 // Console.WriteLine(a.GetType());
 
@@ -6,3 +8,25 @@ var data = new int[] { 1, 2, 3, 4 }
            .Where(e => e > 0)
            .Select(e => new { q = e, w = e + 1 });
 Console.WriteLine(data.GetType());
+
+*/
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"
+              .Replace("(", "")
+              .Replace("(", "")
+              ;
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+                .Select(item => item.Split(','))
+                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+                .Where(e => e.x % 2 == 0)
+                .Select(point => (point.x * 10, point.y))
+                .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
